cc2c1801e4ce3294aec309e984d16a025393b4fa
projec1707 <pwroma@gmail.com>
2023-03-10 15:37:12 +0200

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Roman Riepa on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
